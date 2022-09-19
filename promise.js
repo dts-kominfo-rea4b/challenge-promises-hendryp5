e111896c6cfcd923958cdcb304cd03a67d671fac
hendry priatno <33879584+hendryp5@users.noreply.github.com>
2022-09-19 08:51:51 +0800

    Update promise.js
    update promise

--- promise.js
+++ promise.js
@@ -1,7 +1,17 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
+const promiseOutput = async(emosi) => {
+  const theaterIXX = await promiseTheaterIXX();
+  const theaterVGC = await promiseTheaterVGC();
 
-// TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+  let jumlahEmosi = 0;
+  theaterIXX.forEach(hitung);
+  theaterVGC.forEach(hitung);
+
+  function hitung(item, index) {
+      if (item.hasil == emosi) { jumlahEmosi++ }
+  }
+  return jumlahEmosi;
+};
 
 module.exports = {
   promiseOutput,
